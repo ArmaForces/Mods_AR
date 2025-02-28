945f90d50de9c6763634cc2ef945314103e816e3
Jan Nielek <nielu92@gmail.com>
2025-02-28 14:50:39 +0100

    Fix:Fix ACE Carrying compatibility

--- addons/HotelPvP/scripts/Game/AFM_HotelPvP/UserActions/AFM_CarryHostageAction.c
+++ addons/HotelPvP/scripts/Game/AFM_HotelPvP/UserActions/AFM_CarryHostageAction.c
@@ -1,4 +1,4 @@
-class AFM_CarryHostageAction: ACE_Carrying_BaseUserAction
+class AFM_CarryHostageAction: ACE_Carrying_CarryUserAction
 {
 	override bool CanBeShownScript(IEntity user)
 	{
@@ -11,49 +11,5 @@ class AFM_CarryHostageAction: ACE_Carrying_BaseUserAction
 			return false;
 
 		return true;
-	}
-	
-	override bool CanBePerformedScript(IEntity user)
-	{
-		ChimeraCharacter userChar = ChimeraCharacter.Cast(user);
-		if (!userChar)
-			return false;
-		
-		// Check if user can take crouch stance
-		SCR_CharacterControllerComponent userCharCtrl = SCR_CharacterControllerComponent.Cast(userChar.GetCharacterController());
-		if (!userCharCtrl || !userCharCtrl.CanChangeStance(ECharacterStanceChange.STANCECHANGE_TOCROUCH))
-		{
-			SetCannotPerformReason("#AR-Keybind_StanceProne");
-			return false;
-		}
-		
-		ChimeraCharacter ownerChar = ChimeraCharacter.Cast(GetOwner());
-		if (!ownerChar)
-			return false;
-	
-		
-		// Trying to carry while unit is ragdolling will break things
-		if (ownerChar.GetAnimationComponent().IsRagdollActive())
-		{
-			SetCannotPerformReason("#AR-UserActionUnavailable");
-			return false;
-		}
-
-		return true;
-	}
-	
-	override void PerformAction(IEntity pOwnerEntity, IEntity pUserEntity)
-	{
-		super.PerformAction(pOwnerEntity, pUserEntity);
-		
-		SCR_ChimeraCharacter userChar = SCR_ChimeraCharacter.Cast(pUserEntity);
-		if (!userChar)
-			return;
-		
-		SCR_CharacterControllerComponent userCharController = SCR_CharacterControllerComponent.Cast(userChar.GetCharacterController());
-		if (!userCharController)
-			return;
-		
-		userCharController.ACE_Carrying_CarryCasualty(SCR_ChimeraCharacter.Cast(pOwnerEntity));
 	}
 }
